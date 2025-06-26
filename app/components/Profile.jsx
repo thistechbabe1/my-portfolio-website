ebfa9bd6ba0dae7ca06ecb0eb48269a62e52674f
Sharon Lawal <138617365+SharonLawal@users.noreply.github.com>
2025-06-26 10:07:07 +0100

    Update Profile.jsx

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -88,7 +88,7 @@ const Profile = () => {
             </motion.h1>
           </div>
           <motion.a
-            href="https://drive.google.com/file/d/1Ng3MB8Ia8tmIrB_mE9AWowZUR15M5wrz/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1Ka8EROVyx84YK-XCM8xaD_Q-wypX4eOa/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-6 px-8 py-3 bg-accent-green text-dark-primary font-semibold rounded-lg shadow-lg
